add334d9ddc5eacfadfe7411923fa57952096df4
BuddyL <percent20@gmail.com>
2011-11-17 17:52:39 -0600

    Fixed path issues

--- godjango/settings.py
+++ godjango/settings.py
@@ -108,7 +108,7 @@ MIDDLEWARE_CLASSES = (
 ROOT_URLCONF = 'godjango.urls'
 
 TEMPLATE_DIRS = (
-    "/Users/percent20/Programming/Django/godjango/godjango/templates",
+    "%s/templates" % path('.'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
