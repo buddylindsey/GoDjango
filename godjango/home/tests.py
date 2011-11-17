4b76d088b8b8dea47a54c8aba50f3e97ae88010c
BuddyL <percent20@gmail.com>
2011-11-17 02:35:04 -0600

    added tests for new pages

--- godjango/home/tests.py
+++ godjango/home/tests.py
@@ -9,6 +9,15 @@ class UrlTestClass(unittest.TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
 
-    def test_200_on_video_page(self):
-        response = self.client.get('/1-hello-world/')
+
+    #def test_200_on_video_page(self):
+    #    response = self.client.get('/1-hello-world/')
+    #    self.assertEqual(response.status_code, 200)
+
+    def test_200_on_feedback_page(self):
+        response = self.client.get('/feedback/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_200_on_about_page(self):
+        response = self.client.get('/about/')
         self.assertEqual(response.status_code, 200)
