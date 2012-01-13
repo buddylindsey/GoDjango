604757026ae1b0f490266ee02786a127856bf183
BuddyL <buddy@buddylindsey.com>
2012-01-13 16:57:04 -0600

    tweaked UI with bootstrap

--- godjango/static/js/app.js
+++ godjango/static/js/app.js
@@ -1,14 +1,3 @@
-(function() {
-  var list, number, square;
-
-  list = [1, 2, 3];
-
-  number = 5;
-
-  square = function(x) {
-    return x * x;
-  };
-
-  if (list[0] === 1) document.write('hello');
-
-}).call(this);
+$(document).ready(function(){
+  $('.tabs').tabs();
+});
