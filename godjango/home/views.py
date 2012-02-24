e47ef749bfc49cee01cdc4249e94795e549bd9b3
BuddyL <buddy@buddylindsey.com>
2012-02-23 19:12:21 -0600

    Added Pagination

--- godjango/home/views.py
+++ godjango/home/views.py
@@ -5,12 +5,26 @@ from django.shortcuts import render_to_response
 from django.shortcuts import get_object_or_404
 from django.template import RequestContext
 from django.template.defaultfilters import slugify
+from django.core.paginator import Paginator
 
 from home.models import Video
 from datetime import datetime
 
 def index(request):
-    videos = Video.objects.filter(publish_date__lte=datetime.now()).order_by('-publish_date')[:20]
+    videos_list = Video.objects.filter(publish_date__lte=datetime.now()).order_by('-publish_date')
+    
+    paginator = Paginator(videos_list, 10)
+
+    try:
+        page = int(request.GET.get('page', '1'))
+    except:
+        page = 1
+
+    try:
+        videos = paginator.page(page)
+    except(EmptyPage, InvalidPage):
+        videos = paginator.page(paginator.num_pages)
+
     return render_to_response('home/index.html', 
             { 'videos': videos },
             context_instance=RequestContext(request))
